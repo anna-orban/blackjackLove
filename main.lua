b071ee3976bbdd09a7a0038779a5468f40839aa2
Anna Orban <anna.orban@yahoo.com>
2018-10-08 18:08:31 +0100

    add initial player hand

--- main.lua
+++ main.lua
@@ -6,5 +6,24 @@ function love.load()
             table.insert(deck, {suit = suit, rank = rank})
         end
     end
+
+    playerHand = {}
+    table.insert(playerHand, table.remove(deck, love.math.random(#deck)))
+    table.insert(playerHand, table.remove(deck, love.math.random(#deck)))
+    print('Player hand: ')
+    for cardIndex, card in ipairs(playerHand) do
+        print('suit: '..card.suit..', rank: '..card.rank)
+    end
+
     print('Total number of cards in deck: '..#deck)
+end
+
+function love.draw()
+    local output = {}
+    table.insert(output, 'Player hand: ')
+    for cardIndex, card in ipairs(playerHand) do
+        table.insert( output, 'suit: '..card.suit..', rank: '..card.rank)
+    end
+
+    love.graphics.print(table.concat(output, '\n'), 15, 15)
 end
