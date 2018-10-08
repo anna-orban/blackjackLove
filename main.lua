b17a3749d0153d85883f278709cdc19ced8b490d
Anna Orban <anna.orban@yahoo.com>
2018-10-08 18:56:16 +0100

    calculate hand totals

--- main.lua
+++ main.lua
@@ -23,16 +23,37 @@ function love.load()
 end
 
 function love.draw()
+    function getTotal(hand)
+        local total = 0
+        local hasAce = false
+        for cardIndex, card in ipairs(hand) do
+            if card.rank > 10 then
+                total = total + 10
+            else
+                total = total + card.rank
+            end
+            if card.rank == 1 then
+                hasAce = true
+            end
+        end
+        if hasAce and total <= 11 then
+            total = total + 10
+        end
+        return total
+    end
+
     local output = {}
     table.insert(output, 'Player hand: ')
     for cardIndex, card in ipairs(playerHand) do
         table.insert(output, 'suit: '..card.suit..', rank: '..card.rank)
     end
+    table.insert(output, 'Total: '..getTotal(playerHand))
     table.insert(output, '')
     table.insert(output, 'Dealer hand: ')
     for cardIndex, card in ipairs(dealerHand) do
         table.insert(output, 'suit: '..card.suit..', rank: '..card.rank)
     end
+    table.insert(output, 'Total: '..getTotal(dealerHand))    
 
     love.graphics.print(table.concat(output, '\n'), 15, 15)
 end
